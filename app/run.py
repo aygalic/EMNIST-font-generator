59111677c43f5806a349f01e3942b21a320e6a88
aygalic <aygalic@hotmail.fr>
2024-10-23 18:07:16 +0200

    chore: minor cleanup

--- app/run.py
+++ app/run.py
@@ -1,23 +1,13 @@
-
 import pytorch_lightning as pl
 
 from ef_generator.model import VAE, EMNISTDataModule
-
 from ef_generator.visualization import (
-    visualize_latent_manifold,
-    visualize_samples,
-    visualize_reconstructions,
-    visualize_latent_space,
     visualize_generated_samples,
-    visualize_latent_manifold
+    visualize_latent_manifold,
+    visualize_latent_space,
+    visualize_reconstructions,
+    visualize_samples,
 )
-
-
-
-# Usage example:
-# vae = VAE(latent_dim=26)
-# visualize_latent_manifold(vae)
-
 
 model = VAE()
 data_module = EMNISTDataModule()
@@ -29,7 +19,7 @@ trainer.fit(model, data_module)
 
 visualize_reconstructions(model, data_module, num_samples=5)
 
-# Load the trained model
+
 # visualize_latent_space(model, data_module, n_samples=1000, perplexity=30)
 
 # visualize_generated_samples(model)

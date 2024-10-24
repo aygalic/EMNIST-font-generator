894eba32ffa3177fc251075fbdb1f244cef8cf8c
aygalic <aygalic@hotmail.fr>
2024-10-24 17:46:58 +0200

    chore: dedicated data module: cleanup

--- ef_generator/model.py
+++ ef_generator/model.py
@@ -2,25 +2,24 @@ import pytorch_lightning as pl
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-from torch.utils.data import DataLoader, random_split
-from torchvision import transforms
-from torchvision.datasets import EMNIST
 
 class VAE(pl.LightningModule):
-    def __init__(self, latent_dim=4):
+    def __init__(self, latent_dim=8):
         super().__init__()
         self.latent_dim = latent_dim
         self.dropout = 0
 
+        self.feature_multiplier = 4
+
         # Encoder
         self.encoder = nn.Sequential(
-            nn.Conv2d(1, 16, 3, stride=2, padding=1),
+            nn.Conv2d(1, 16*self.feature_multiplier, 3, stride=2, padding=1),
             nn.ReLU(),
             nn.Dropout(self.dropout),
-            nn.Conv2d(16, 32, 3, stride=2, padding=1),
+            nn.Conv2d(16*self.feature_multiplier, 32*self.feature_multiplier, 3, stride=2, padding=1),
             nn.ReLU(),
             nn.Dropout(self.dropout),
-            nn.Conv2d(32, 64, 7),
+            nn.Conv2d(32*self.feature_multiplier, 64, 7),
             nn.ReLU(),
             nn.Dropout(self.dropout),
             nn.Flatten(),
@@ -34,13 +33,13 @@ class VAE(pl.LightningModule):
         self.decoder_input = nn.Linear(self.latent_dim, 64)
         self.decoder = nn.Sequential(
             nn.Unflatten(1, (64, 1, 1)),
-            nn.ConvTranspose2d(64, 32, 7),
+            nn.ConvTranspose2d(64, 32*self.feature_multiplier, 7),
             nn.ReLU(),
             nn.Dropout(self.dropout),
-            nn.ConvTranspose2d(32, 16, 3, stride=2, padding=1, output_padding=1),
+            nn.ConvTranspose2d(32*self.feature_multiplier, 16*self.feature_multiplier, 3, stride=2, padding=1, output_padding=1),
             nn.ReLU(),
             nn.Dropout(self.dropout),
-            nn.ConvTranspose2d(16, 1, 3, stride=2, padding=1, output_padding=1),
+            nn.ConvTranspose2d(16*self.feature_multiplier, 1, 3, stride=2, padding=1, output_padding=1),
             nn.Sigmoid()
         )
 
@@ -85,33 +84,3 @@ class VAE(pl.LightningModule):
         optimizer = torch.optim.Adam(self.parameters(), lr=1e-3)
         return optimizer
 
-class EMNISTDataModule(pl.LightningDataModule):
-    def __init__(self, data_dir: str = "./data", batch_size: int = 32):
-        super().__init__()
-        self.data_dir = data_dir
-        self.batch_size = batch_size
-        self.transform = transforms.Compose([
-            transforms.ToTensor(),
-            transforms.Normalize((0.1307,), (0.3081,))
-        ])
-
-    def prepare_data(self):
-        EMNIST(self.data_dir, split='letters', train=True, download=True)
-        EMNIST(self.data_dir, split='letters', train=False, download=True)
-
-    def setup(self, stage=None):
-        if stage == 'fit' or stage is None:
-            emnist_full = EMNIST(self.data_dir, split='letters', train=True, transform=self.transform)
-            self.emnist_train, self.emnist_val = random_split(emnist_full, [0.9, 0.1])
-
-        if stage == 'test' or stage is None:
-            self.emnist_test = EMNIST(self.data_dir, split='letters', train=False, transform=self.transform)
-
-    def train_dataloader(self):
-        return DataLoader(self.emnist_train, batch_size=self.batch_size, shuffle=True)
-
-    def val_dataloader(self):
-        return DataLoader(self.emnist_val, batch_size=self.batch_size)
-
-    def test_dataloader(self):
-        return DataLoader(self.emnist_test, batch_size=self.batch_size)

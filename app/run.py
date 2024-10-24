9969dd59324ef011d64b666919c7e0ce5ba62af9
aygalic <aygalic@hotmail.fr>
2024-10-24 17:48:00 +0200

    chore: better granularity controlling model parameters

--- app/run.py
+++ app/run.py
@@ -9,19 +9,27 @@ from ef_generator.visualization import (
     visualize_reconstructions,
     visualize_samples,
 )
+from ef_generator.callbacks import ValidationLossCallback
+
+
 
 model = VAE()
 data_module = EMNISTDataModule()
 
+
 # visualize_samples(data_module, num_samples=25, cols=5)
 
-trainer = pl.Trainer(max_epochs=3, precision="16-mixed")
+trainer = pl.Trainer(
+    max_epochs=3,
+    precision="16-mixed",
+    callbacks=[ValidationLossCallback(print_epoch=True, decimal_places=4)],
+    )
 trainer.fit(model, data_module)
 
 visualize_reconstructions(model, data_module, num_samples=5)
 
 
-# visualize_latent_space(model, data_module, n_samples=1000, perplexity=30)
+visualize_latent_space(model, data_module, n_samples=1000, perplexity=30)
 
 # visualize_generated_samples(model)
 
